2513e6deb28a68baf21e45341a2c3e0d9c6a7738
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-04-23 21:32:13 +0530

    update

--- DjangoPrograms/formproject/testapp/forms.py
+++ DjangoPrograms/formproject/testapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
 class StudentForm(forms.Form):
+  Rollno = forms.IntegerField()
   Name = forms.CharField() #no need to specify max-lenght
   Marks = forms.IntegerField()
--- DjangoPrograms/formproject/testapp/views.py
+++ DjangoPrograms/formproject/testapp/views.py
@@ -4,6 +4,18 @@ from testapp.forms import StudentForm
 # Create your views here.
 
 def student_input_view(request):
+                submitted = False
+                sname = " "
+                
+                if request.method == "POST":
+                        form = StudentForm(request.POST)
+                        if form.is_valid():
+                         print("FOrm Validaiton is Succesfull and print data")
+                         print("Rollno : ",form.cleaned_data["Rollno"])
+                         print("Name : ",form.cleaned_data["Name"])
+                         print("Marks :",form.cleaned_data["Marks"])
+                         submitted = True
+                         sname = form.cleaned_data["Name"]
+              
                 form = StudentForm()
-                mydict = {"form":form}
-                return render (request,"html/index.html",context=mydict)
+                return render (request,"html/index.html",context={"form":form,"submitted":submitted,"sname":sname})
