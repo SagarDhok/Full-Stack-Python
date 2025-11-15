e32599192d0abc6384bfe831c65135a04773bf0e
sagardhok <sdhok041@gmail.com>
2025-11-15 13:39:47 +0530

    Update

--- DjangoPrograms/OrmProject/testapp/admin.py
+++ DjangoPrograms/OrmProject/testapp/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from testapp.models import Employee
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['eno','ename','esal','eaddr']
+    list_display = ["id",'eno','ename','esal','eaddr']
 admin.site.register(Employee,EmployeeAdmin)
--- DjangoPrograms/OrmProject/testapp/views.py
+++ DjangoPrograms/OrmProject/testapp/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
+from django.db.models import Q
 
 # Create your views here.
 
 from testapp.models import Employee
 def retrieve_view(request):
-    emp_list = Employee.objects.all()
-    print(type(emp_list))
-   # <class 'django.db.models.query.QuerySet'>
-    return render(request,'testapp/index.html',{'emp_list':emp_list})
+    # emp_list = Employee.objects.all()
+    # emp_list = Employee.objects.get(id__exact=51)
+    # emp_list = [Employee.objects.get(id=51)] #!1. get() → single object Not iterable  → loop nahi chalega
+    #!loop chalvayha asel tr list madhe
+    # emp_list = Employee.objects.filter(esal__gte = 15000)
+    # emp_list = Employee.objects.filter(esal__lt=11000)
+    # emp_list = Employee.objects.filter(ename__contains = 'su')
+    # emp_list = Employee.objects.filter(eno__contains = '1')
+    # emp_list = Employee.objects.filter(id__in = [51,1,5])
+    # emp_list = Employee.objects.filter(ename__startswith="A")
+    # emp_list = Employee.objects.filter(ename__endswith="r")
+    # emp_list = Employee.objects.filter(esal__range=[10000,15000])
+    # emp_list = Employee.objects.filter(ename__startswith="S")| Employee.objects.filter(esal__lt = 10001)
+    # emp_list = Employee.objects.filter(Q(ename__startswith="A")|Q(esal__lt = 10001))
+    # emp_list = Employee.objects.filter(ename__startswith="S")& Employee.objects.filter(esal__lt = 15000)
+    # emp_list = Employee.objects.filter(Q(ename__startswith="S")& Q(esal__lt = 18000))
+    # emp_list = Employee.objects.filter(ename__startswith="S",esal__lt = 15000)
+    # emp_list = Employee.objects.exclude(ename__startswith="m")
+    # emp_list = Employee.objects.exclude()
+    # emp_list = Employee.objects.filter(~Q(ename__startswith = "s"))
+    # emp_list = Employee.objects.filter(~Q(esal__gte=15000))
+    # emp_list = Employee.objects.all().values_list("ename",'esal')
+    # emp_list = Employee.objects.all().values("ename",'esal')
+    emp_list = Employee.objects.all().only("ename",'esal')
+    return render(request,'testapp/specificcolumns.html',{'emp_list':emp_list})
+
+
+    
+
+
+
+    # print(type(emp_list))
+    # return render(request,'testapp/index.html',{'emp_list':emp_list})
