5926ec480840b20d3ea85fdf0f93cdcb933c43e1
sagardhok <sdhok041@gmail.com>
2025-11-20 18:33:38 +0530

    Update

---
+++ Backend/RESTApiPrograms/withrestc4/testapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework.viewsets import ModelViewSet
+from testapp.models import Employee
+from testapp.serializers import EmployeeSerializer
+
+class EmplyeeCRUDCBV(ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
